0c71964d09a6411f9aaaaefeed96b53f44d5938e
Sergey <padawanque@users.noreply.github.com>
2020-11-19 22:39:27 +0300

    Create docker-init.sh
    inite release

---
+++ docker-init.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# https://docs.docker.com/engine/install/ubuntu/
+
+apt update
+
+apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+	
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt update
+apt install docker-ce docker-ce-cli containerd.io
+
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+groupadd docker
+usermod -aG docker $USER
